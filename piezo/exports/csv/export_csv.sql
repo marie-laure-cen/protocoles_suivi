278628b829ff8b5255967eb04fe037b4484a164f
marie-laure-cen <mls@cen-normandie.fr>
2024-07-04 11:39:15 +0200

    piezo module configuration

--- piezo/exports/csv/export_csv.sql
+++ piezo/exports/csv/export_csv.sql
@@ -1,11 +1,6 @@
--- alter table gn_monitoring.t_base_sites alter column id_nomenclature_type_site drop not null;
+DROP VIEW  IF EXISTS  gn_monitoring.v_export_piezo_visit;
 
--------------------------------------------------final --STERF standard------------------------------------------
--- View: gn_monitoring.v_export_terf_standard
-
-DROP VIEW  IF EXISTS  gn_monitoring.v_export_terf_standard;
-
-CREATE OR REPLACE VIEW gn_monitoring.v_export_sterf_standard AS
+CREATE OR REPLACE VIEW gn_monitoring.v_export_piezo_visit AS
 
 WITH source AS (
 		SELECT
@@ -13,117 +8,102 @@ WITH source AS (
 		FROM gn_synthese.t_sources
 		WHERE name_source = CONCAT('MONITORING_', UPPER(:'module_code'))
 		LIMIT 1
-	),sites AS (
-         SELECT tbs.id_base_site,
-            tsg.sites_group_code::integer AS id_dataset,
-            taf.dataset_name,
-            la.id_area AS id_area_attachment,
-            cafs.area_code,
-            la.area_name,
-            (tbs.base_site_code::text || ' / '::text) || tbs.base_site_name::text AS transect,
-            (r.nom_role::text || ' '::text) || r.prenom_role::text AS responsable,
-            (tsc.data -> 'lisiere'::text)::text AS lisiere,
-            tsc.data -> 'hab_1'::text AS hab_1,
-            (tsc.data -> 'hab_2'::text)::text AS hab_2,
-            tbs.base_site_description AS site_comments,
-            tbs.altitude_min,
-            tbs.altitude_max,
-            tbs.geom AS the_geom_4326,
-            st_centroid(tbs.geom) AS the_geom_point,
-            tbs.geom_local
-           FROM gn_monitoring.t_base_sites tbs
-             LEFT JOIN gn_monitoring.t_site_complements tsc USING (id_base_site)
-             LEFT JOIN gn_monitoring.t_sites_groups tsg USING (id_sites_group)
-             LEFT JOIN utilisateurs.t_roles r ON (tsc.data -> 'id_resp'::text)::integer = r.id_role
-             LEFT JOIN gn_meta.t_datasets taf ON taf.id_dataset = tsg.sites_group_code::integer
-             LEFT JOIN gn_meta.cor_acquisition_framework_site cafs USING (id_acquisition_framework)
-             LEFT JOIN ref_geo.l_areas la USING (area_code)
-        ), visits AS (
-         SELECT tbv.id_base_visit,
-            tbv.uuid_base_visit,
-            tbv.id_module,
-            tbv.id_base_site,
-            tbv.id_digitiser,
-            (tbv.visit_date_min::text || ((tvc.data -> 'heure_debut'::text)::text))::timestamp without time zone AS date_min,
-            COALESCE((tbv.visit_date_max::text || ((tvc.data -> 'heure_fin'::text)::text))::timestamp without time zone, (tbv.visit_date_min::text || ((tvc.data -> 'heure_fin'::text)::text))::timestamp without time zone) AS date_max,
-            tbv.comments,
-            tbv.id_nomenclature_tech_collect_campanule,
-            tbv.id_nomenclature_grp_typ,
-            (tvc.data -> 'num_passage'::text)::integer AS num_passage,
-            ref_nomenclatures.get_nomenclature_label((tvc.data -> 'id_nomenclature_tp'::text)::integer) AS temperature,
-            ref_nomenclatures.get_nomenclature_label((tvc.data -> 'id_nomenclature_cn'::text)::integer) AS couv_nuageuse,
-            ref_nomenclatures.get_nomenclature_label((tvc.data -> 'id_nomenclature_vt'::text)::integer) AS vent,
-            (tvc.data -> 'source'::text)::text AS srce
-           FROM gn_monitoring.t_base_visits tbv
-             LEFT JOIN gn_monitoring.t_visit_complements tvc USING (id_base_visit)
-        ), observers AS (
-         SELECT array_agg(r.id_role) AS ids_observers,
-            string_agg(concat(r.nom_role, ' ', r.prenom_role), ' ; '::text) AS observers,
-            cvo.id_base_visit
-           FROM gn_monitoring.cor_visit_observer cvo
-             JOIN utilisateurs.t_roles r ON r.id_role = cvo.id_role
-          GROUP BY cvo.id_base_visit
-        )
- SELECT o.uuid_observation AS unique_id_sinp,
-    v.uuid_base_visit AS unique_id_sinp_grp,
-    s.area_code AS id_site,
-    s.area_name AS nom_site,
-    s.id_dataset,
-    s.dataset_name,
-    m.module_code AS suivi,
-    s.responsable,
-    s.transect,
+	),
+  sites AS (
+    SELECT 
+      tbs.id_base_site,
+      tsg.sites_group_code::integer AS id_site,
+      tbs.base_site_code as piezo,
+      tbs.base_site_name as protocole,
+      (tsc.data -> 'modele')::text as modele,
+      tbs.base_site_description AS site_comments,
+      tbs.altitude_min,
+      tbs.altitude_max,
+      tbs.geom AS the_geom_4326,
+      st_centroid(tbs.geom) AS the_geom_point,
+      tbs.geom_local
+      FROM gn_monitoring.t_base_sites tbs
+        LEFT JOIN gn_monitoring.t_site_complements tsc USING (id_base_site)
+        LEFT JOIN gn_monitoring.t_sites_groups tsg USING (id_sites_group)
+  ),
+  observers AS (
+    SELECT array_agg(r.id_role) AS ids_observers,
+      string_agg(concat(r.nom_role, ' ', r.prenom_role), ' ; '::text) AS observers,
+      cvo.id_base_visit
+    FROM gn_monitoring.cor_visit_observer cvo
+      JOIN utilisateurs.t_roles r ON r.id_role = cvo.id_role
+    GROUP BY cvo.id_base_visit
+  )
+ SELECT 
+    -- piézomètre
+    s.id_site,
+    s.piezo,
+    s.protocole,
+    s.modele,
     s.altitude_min,
     s.altitude_max,
-        CASE
-            WHEN s.lisiere = 'Oui'::text THEN true
-            ELSE false
-        END AS lisiere,
-    s.hab_1,
-    s.hab_2,
-    date_part('year'::text, v.date_min) AS annee,
-    v.num_passage,
-    v.date_min,
-    v.date_max,
-    v.id_digitiser,
-    v.temperature,
-    v.couv_nuageuse,
-    v.vent,
-    o.cd_nom,
-    t.cd_ref,
-    t.nom_complet,
-    t.regne,
-    t.phylum,
-    t.classe,
-    t.ordre,
-    t.famille,
-    (oc.data -> 'effectif'::text)::integer AS effectif,
-    obs.observers,
-    oc.data -> 'determiner'::text AS determiner,
-    'Stationnel'::text AS geo_object_nature,
-    ref_nomenclatures.get_nomenclature_label(v.id_nomenclature_grp_typ) AS grp_typ,
-    ref_nomenclatures.get_nomenclature_label(v.id_nomenclature_tech_collect_campanule) AS tech_collect_campanule,
-    ref_nomenclatures.get_nomenclature_label((oc.data -> 'id_nomenclature_determination_method'::text)::integer) AS determination_method,
-    ref_nomenclatures.get_nomenclature_label((oc.data -> 'id_nomenclature_obs_technique'::text)::integer) AS obs_technique,
-    ref_nomenclatures.get_nomenclature_label((oc.data -> 'id_nomenclature_obj_count'::text)::integer) AS obj_count,
-    ref_nomenclatures.get_nomenclature_label((oc.data -> 'id_nomenclature_type_count'::text)::integer) AS _type_count,
-    'Présent'::text AS observation_status,
+    -- sondages
+    date_part('year'::text, v.visit_date_min) AS annee,
+    (tvc.data -> 'num_sondage') as num_sondage,
+    v.visit_date_min as date_sondage,
+    (digit.nom_role || ' ' || digit.prenom_role) as numerisateur_sondage,
+    utils.observers as observateurs_sondage,
     'Terrain'::text AS source_status,
     'Géoréférencement'::text AS info_geo_type,
-    (s.site_comments || ' / '::text) || v.comments AS comment_context,
-    o.comments AS comment_description,
-    v.srce,
+    s.site_comments || ' / ' || v.comments AS comment_context,
+    -- Infos générales
+    m.module_code AS suivi,
+    v.uuid_base_visit AS unique_id_sinp_grp,
+    v.id_dataset,
+    -- géométrie
     s.the_geom_4326,
     s.the_geom_point,
     s.geom_local
-   FROM gn_monitoring.t_observations o
-     LEFT JOIN gn_monitoring.t_observation_complements oc USING (id_observation)
-     JOIN visits v USING (id_base_visit)
-     JOIN sites s USING (id_base_site)
-     JOIN gn_commons.t_modules m USING (id_module)
-     JOIN taxonomie.taxref t USING (cd_nom)
-     JOIN source ON true
-     JOIN observers obs USING (id_base_visit)
-    WHERE m.module_code = :'module_code'
-  ORDER BY s.area_code, s.transect, (date_part('year'::text, v.date_min)), v.num_passage, o.cd_nom;
-    ;
+    FROM gn_monitoring.t_base_visits v
+      LEFT JOIN gn_monitoring.t_visit_complements tvc USING (id_base_visit)
+      JOIN sites s USING (id_base_site)
+      JOIN gn_commons.t_modules m USING (id_module)
+      JOIN source ON true
+      JOIN observers utils USING (id_base_visit)
+      LEFT JOIN utilisateurs.t_roles digit ON id_digitiser = id_role
+    WHERE m.module_code =  :'module_code'
+  ORDER BY s.id_site, s.piezo, (date_part('year'::text, v.visit_date_min)), (tvc.data -> 'num_sondage') 
+  ;
+  
+  -- View: gn_monitoring.v_export_piezo_site
+
+DROP VIEW  IF EXISTS  gn_monitoring.v_export_piezo_site;
+
+CREATE OR REPLACE VIEW gn_monitoring.v_export_piezo_site AS
+
+WITH source AS (
+		SELECT
+			id_source
+		FROM gn_synthese.t_sources
+		WHERE name_source = CONCAT('MONITORING_', UPPER(:'module_code'))
+		LIMIT 1
+	)
+ SELECT 
+    -- piézomètre
+	tsg.sites_group_code AS id_site,
+	tbs.base_site_code as piezo,
+    tbs.base_site_name as protocole,
+    (tsc.data -> 'modele')::text as modele,
+    tbs.base_site_description AS site_comments,
+    tbs.altitude_min,
+    tbs.altitude_max,
+    -- Infos générales
+    m.module_code AS suivi,
+    tbs.id_base_site AS unique_id_piezo,
+    -- géométrie
+	tbs.geom AS the_geom_4326,
+	st_centroid(tbs.geom) AS the_geom_point,
+	tbs.geom_local
+    FROM gn_monitoring.t_base_sites tbs
+	LEFT JOIN gn_monitoring.t_site_complements tsc USING (id_base_site)
+	LEFT JOIN gn_monitoring.t_sites_groups tsg USING (id_sites_group)
+	JOIN gn_commons.t_modules m ON tsg.id_module = m.id_module
+	JOIN source ON true
+    WHERE m.module_code =  :'module_code'
+	ORDER BY tsg.sites_group_code, tbs.base_site_code
+	;
--- piezo/synthese.sql
+++ piezo/synthese.sql
@@ -31,15 +31,11 @@ WITH source AS (
 	sites AS (
 		SELECT
 			tbs.id_base_site,
-			tsg.sites_group_code::integer as id_dataset,
-			la.id_area as id_area_attachment,
-			tsg.sites_group_name as area_code,
-			(tbs.base_site_code || ' / ' || tbs.base_site_name ) as place_name,
-			(r.nom_role || ' ' || r.prenom_role) as responsable,
+			tsg.sites_group_name as id_site,
+     		tbs.base_site_code as piezo,
+      		tbs.base_site_name as protocole,
+			tbs.base_site_description as comments,
 			tsc.data,
-			(tsc.data->'lisiere')::text as lisiere,
-			(tsc.data->'hab_1'::text) as hab_1,
-			(tsc.data->'hab_2')::text as hab_2,
 			tbs.base_site_description as site_comments,
 			tbs.altitude_min,
 			tbs.altitude_max,
@@ -49,34 +45,6 @@ WITH source AS (
         FROM gn_monitoring.t_base_sites tbs
 		LEFT JOIN gn_monitoring.t_site_complements tsc USING (id_base_site)
 		LEFT JOIN gn_monitoring.t_sites_groups tsg USING (id_sites_group)
-		LEFT JOIN utilisateurs.t_roles r ON (tsg.data->'id_resp' )::integer = id_role
-		LEFT JOIN gn_meta.t_datasets taf ON taf.id_dataset = tsg.sites_group_code::integer
-		--LEFT JOIN gn_meta.cor_acquisition_framework_site cafs USING (id_acquisition_framework)
-		LEFT JOIN ref_geo.l_areas la ON tsg.sites_group_name = la.area_code
-	),
-	visits AS (
-		SELECT    
-			tbv.id_base_visit,
-			tbv.uuid_base_visit,
-			tbv.id_module,
-			tbv.id_base_site,
-			tbv.id_digitiser,
-			(tbv.visit_date_min::text || (tvc.data->'heure_debut')::text )::timestamp AS date_min,
-			COALESCE (
-				(tbv.visit_date_max::text || (tvc.data->'heure_fin')::text )::timestamp, 
-				(tbv.visit_date_min::text || (tvc.data->'heure_fin')::text )::timestamp
-			)::timestamp AS date_max,
-			tbv.comments,
-			tbv.id_nomenclature_tech_collect_campanule,
-			tbv.id_nomenclature_grp_typ,
-			tvc.data,
-			(tvc.data->'num_passage')::integer as num_passage,
-			ref_nomenclatures.get_nomenclature_label((tvc.data->'id_nomenclature_tp')::integer) as temperature,
-			ref_nomenclatures.get_nomenclature_label((tvc.data->'id_nomenclature_cn')::integer) as couv_nuageuse,
-			ref_nomenclatures.get_nomenclature_label((tvc.data->'id_nomenclature_vt')::integer) as vent,
-			(tvc.data->'source')::text as srce
-        FROM gn_monitoring.t_base_visits tbv
-		LEFT JOIN gn_monitoring.t_visit_complements tvc USING (id_base_visit)
 	),
 	observers AS (
 		SELECT
@@ -91,69 +59,36 @@ WITH source AS (
 	SELECT
 		-- source ids
 		v.id_module,
-		o.uuid_observation AS unique_id_sinp, 
-		v.uuid_base_visit AS unique_id_sinp_grp,
 		source.id_source,
-		o.id_observation AS entity_source_pk_value,
 		v.id_base_site,
 		v.id_base_visit,
-		s.id_area_attachment,
-		-- site and transect informations
-		s.id_dataset,
-		s.area_code as site_cen,
-		s.place_name,
-		s.responsable,
+		v.uuid_base_visit AS unique_id_sinp_grp,
+		-- piézomètres
+		s.id_site,
+		s.piezo,
 		s.altitude_min,
 		s.altitude_max,
-		-- visit informations
-		v.num_passage,
-		extract(year from v.date_min) as annee,
-		v.date_min,
-		v.date_max, 
+		(s.comments || ' protocole : ' || s.protocole) as site_comments,
+		-- sondages
+		(tvc.data -> 'num_sondage') as num_sondage,
+		extract(year from v.visit_date_min) as annee,
+		v.visit_date_min,
 		v.id_digitiser,
-        ref_nomenclatures.get_id_nomenclature('NAT_OBJ_GEO', 'St') AS id_nomenclature_geo_object_nature,
-		ref_nomenclatures.get_id_nomenclature('TYP_GRP', 'PASS') AS id_nomenclature_grp_typ,
-		ref_nomenclatures.get_id_nomenclature('TECHNIQUE_OBS', '59') AS id_nomenclature_tech_collect_campanule,
-		-- observation informations
-		(oc.data->'effectif')::integer as count_min,
-		(oc.data->'effectif')::integer as count_max,
-		o.cd_nom,
-		t.nom_complet AS nom_cite,
-		obs.observers,
-		oc.data->'determiner' as determiner,
-		(oc.data->'id_nomenclature_determination_method')::integer  as id_nomenclature_determination_method,
-		(oc.data->'id_nomenclature_obs_technique')::integer  as id_nomenclature_obs_technique,
-		(oc.data->'id_nomenclature_obj_count')::integer as id_nomenclature_obj_count,
-		(oc.data->'id_nomenclature_type_count')::integer as id_nomenclature_type_count,
- 		ref_nomenclatures.get_id_nomenclature('STATUT_OBS', 'Pr') AS id_nomenclature_observation_status, 
-		ref_nomenclatures.get_id_nomenclature('STATUT_SOURCE', 'Te') AS id_nomenclature_source_status,
-		ref_nomenclatures.get_id_nomenclature('TYP_INF_GEO', '1') AS id_nomenclature_info_geo_type,
-		--comments
-		(s.site_comments || ' / ' || v.comments) AS comment_context,
-		o.comments AS comment_description,
-		-- additional data
-		(CASE WHEN s.lisiere = 'Oui' THEN TRUE ELSE FALSE END) as lisiere,
-		s.hab_1,
-		s.hab_2,
-		v.temperature,
-		v.couv_nuageuse,
-		v.vent,
-		(oc.data->'nb_male') as nb_male,
-		(oc.data->'nb_femelle') as nb_femelle,
-		-- geometry
+    	v.id_dataset,
+		-- données additionnelles
+		jsonb_strip_nulls(COALESCE(s.data || tvc.data , tvc.data, s.data)) as additional_data,
+		-- géométries
 		s.the_geom_4326,
 		s.the_geom_point,
-		s.geom_local,
-		jsonb_strip_nulls(COALESCE(s.data || v.data || oc.data , oc.data, v.data, s.data)) as additional_data
-	FROM gn_monitoring.t_observations o
-	LEFT JOIN gn_monitoring.t_observation_complements oc using (id_observation)
-    JOIN visits v USING (id_base_visit)
+		s.geom_local
+	FROM gn_monitoring.t_base_visits v
+	LEFT JOIN gn_monitoring.t_visit_complements tvc USING (id_base_visit)
     JOIN sites s USING (id_base_site)
 	JOIN gn_commons.t_modules m USING (id_module)
-	JOIN taxonomie.taxref t USING (cd_nom)
 	JOIN source ON TRUE
 	JOIN observers obs USING (id_base_visit)
-    WHERE m.module_code = :'module_code' and not v.srce = 'Intranet SER'
+    WHERE m.module_code = :'module_code'
 ;
 
 SELECT * FROM gn_monitoring.v_synthese_:module_code
+;
